e761b9cd126a9cd3ff758a8537b768974a12e72d
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-08-23 19:04:10 +0200

    proxy cors

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let index = 1;
 
-let url = "http://xkcd.com/" + index + "/info.0.json"
+let url = "https://led6jnh0vc.execute-api.eu-west-1.amazonaws.com/prod?url=https://xkcd.com/" + window.location.search.replace("?","") + "/info.0.json"
 
 const comic = document.getElementById("comic")
 
 const data = fetch(url).then(x => x.json()).then(y=> {
-
+    comic.src = y.img
     console.log(y)
 })
